ff9c6594922b6f1ab47facd372514bc01dfb0d09
ChrisMak <cmakride@gmail.com>
2022-03-08 20:15:32 -0500

    Flashcard Tab, Pick Rand Cocktail Initialized

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,1 +1,23 @@
-console.log("SANITY")
+let array = new Array(7).fill(null)
+
+
+init()
+//cached element references
+
+//make all classes hidden
+
+function init(){
+//pick a random cocktail
+let num = Math.floor(Math.random() * array.length)
+console.log(num)
+  //Make each cocktail card hidden
+array.forEach((div,idx)=>{
+const element = document.querySelector(`#cocktail_0${idx}`)
+if(idx === num){
+  element.className = "wrapper"
+}
+})
+//pick a random cocktail
+
+
+}
